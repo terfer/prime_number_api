83d229dfc065430eefc440b01af853e798e2f8dd
Fermin <ferminca.tic@gmail.com>
2022-05-07 17:30:13 +0000

    Fix pylint errors C0103, W1309

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 try:
-    open(f'data/list.data', 'r').readlines()[-1]
+    open('data/list.data', 'r').readlines()[-1]
 except:
     db = open('data/list.data', 'w')
     db.write("1")
     db.close()
 
 
-def esPrimo(num: int):
+def is_prime(num: int):
     for i in range(2, int(num/2)+1):
         if ((num % i) == 0) & (num != i):
             return False
@@ -19,15 +19,15 @@ def esPrimo(num: int):
 
 @app.route('/queue', methods=['POST'])
 def calculate_num_prime():
-    first_number_calculated = int(open(f'data/list.data', 'r').readlines()[-1])
-    data_new = len(open(f'data/list.data', 'r').read())
+    first_number_calculated = int(open('data/list.data', 'r').readlines()[-1])
+    data_new = len(open('data/list.data', 'r').read())
     db = open(f'data/list.data', 'a')
     first_number_calculated += 1
     if not request.json or not 'num_calc' in request.json:
         abort(400)
     number_calculations = int(request.json['num_calc']) + data_new
     while data_new < number_calculations:
-        if esPrimo(first_number_calculated):
+        if is_prime(first_number_calculated):
             db.write("\n")
             db.write(str(first_number_calculated))
             data_new += 1
@@ -38,7 +38,7 @@ def calculate_num_prime():
 
 @app.route('/get_primes', methods=['GET'])
 def get_prime_numbers():
-    db = open(f'data/list.data', 'r')
+    db = open('data/list.data', 'r')
     return str(db.read())
 
 
